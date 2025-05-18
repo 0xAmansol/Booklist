1cf5cfb35f80cc3fc76d6d1634d009d8156f3636
Aman Kalal <amankalal206@gmail.com>
2025-05-19 03:17:52 +0530

    made login ssr

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { supabase } from "@/lib/supabase";
+import dynamic from "next/dynamic";
 
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow">
-        <h1 className="text-2xl font-bold mb-6 text-center font-instrument-serif text-emerald-600">
-          Sign in to BookList
-        </h1>
-        <Auth
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          providers={["google", "github"]}
-          redirectTo={`${window.location.origin}/dashboard/leaderboard`}
-        />
-      </div>
-    </div>
-  );
+const LoginPage = dynamic(() => import("@/components/LoginPage"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <LoginPage />;
 }
---
+++ components/LoginPage.tsx
@@ -0,0 +1,25 @@
+import { supabase } from "@/lib/supabase";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <div className="w-full max-w-md p-8 bg-white rounded shadow">
+        <h1 className="text-2xl font-bold mb-6 text-center font-instrument-serif text-emerald-600">
+          Sign in to BookList
+        </h1>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={["google", "github"]}
+          redirectTo={`${window.location.origin}/dashboard/leaderboard`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
