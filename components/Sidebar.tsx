d5c90c96d3a600bd7ff4bb6c05108c080420c13d
Aman Kalal <amankalal206@gmail.com>
2025-05-19 08:42:26 +0530

    added Image config

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
         <div className="flex items-center justify-start mb-6">
           <Link href="/" className="flex items-center gap-1 pl-2">
             <Image
-              src="/BookList.png"
+              src="/booklist.png"
               alt="BookList Logo"
               width={40}
               height={40}
